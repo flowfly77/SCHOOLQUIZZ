104c8f51edaef2e9b5d9ee7407d809d577cfe5f2
flowfly77 <flowfly77700@gmail.com>
2025-10-15 10:00:29 +0200

    Delete js directory

--- js/firebase.js
+++
@@ -1,7 +0,0 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-export const app = initializeApp(window.FB_CONFIG);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
--- js/ui.js
+++
@@ -1,7 +0,0 @@
-// js/ui.js
-export const $ = (s)=>document.querySelector(s);
-export function show(id){ document.querySelectorAll('.screen').forEach(s=>s.classList.remove('active')); $(id).classList.add('active'); }
-export function showAuth(){ show('#auth-screen'); }
-export function showProfiles(){ show('#profiles-screen'); }
-export function showApp(){ show('#app-screen'); }
-export const msg = (t)=> document.getElementById('auth-msg').textContent = t || '';
